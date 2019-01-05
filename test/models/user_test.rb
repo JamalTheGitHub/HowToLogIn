22a0344d992862ddc4bc2a73b8713248947da35e
User <ezzy_nauzzee@hotmail.com>
2019-01-05 08:38:09 +0800

    Did simple test for user model. Also added resources :users to routes.rb. Added create action to users controller as well as user_params under private in users controller. All in all, sign up works. Readme steps is not complete yet and will be completed soon.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "james@gmail.com" , password: "123")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end
